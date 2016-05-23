c89c588c83c9e31fe89300027dbbdf18400eb03f
Henrik Lissner <henrik@lissner.net>
2016-05-23 05:23:34 -0400

    Add doom-one-light stub

---
+++ doom-one-light-theme.el
@@ -0,0 +1,14 @@
+;; DOOM One Light (inspired by Atom One Light)
+
+(require 'doom)
+
+;; (doom-init 'doom-one-light)
+
+;; TODO
+;; Nothing here yet!
+
+(provide-theme 'doom-one-light)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
