e5af48104304312f2d8d3b734d99cfbc5d6da047
Henrik Lissner <henrik@lissner.net>
2016-09-23 15:43:08 +0200

    v1.1.0 bump

--- doom-themes.el
+++ doom-themes.el
@@ -5,8 +5,8 @@
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Created: May 22, 2016
-;; Modified: September 15, 2016
-;; Version: 1.0.9
+;; Modified: September 23, 2016
+;; Version: 1.1.0
 ;; Keywords: dark blue atom one seek
 ;; Homepage: https://github.com/hlissner/emacs-doom-theme
 ;; Package-Requires: ((emacs "24.4") (dash "2.12.0") (all-the-icons "1.0.0"))
