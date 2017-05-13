78db30c0a1fd75a3ddbdbc70c3509cfa0304427b
Henrik Lissner <henrik@lissner.net>
2017-05-13 12:05:16 +0200

    doom-one: vc-modified = light-grey
    green/yellow contrast isn't high enough in some cases

--- doom-one-theme.el
+++ doom-one-theme.el
@@ -74,7 +74,7 @@ determine the exact padding."
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    yellow)
+   (vc-modified    light-grey)
    (vc-added       green)
    (vc-deleted     red)
 
