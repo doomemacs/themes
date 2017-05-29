59ba769cad7d751765be734185866e2525f09fcf
Christian Kellner <christian@kellner.me>
2017-05-29 18:11:58 +0200

    Add lsp faces
    Faces for the lsp mode[1], which currently are:
    
      - lsp-face-highlight-textual
          "textual occurances of symbols."
          default: yellow background
    
      - lsp-face-hightlight-read
          "highlighting symbols being read."
          default: red background
    
      - lsp-face-hightlight-write
          "highlighting symbols being written to."
          default: green background
    
    [1] https://github.com/emacs-lsp/lsp-mode

--- doom-themes-common.el
+++ doom-themes-common.el
@@ -441,6 +441,11 @@ Faces in EXTRA-FACES override the default faces."
        ;; nlinum-relative
        (nlinum-relative-current-face :inherit 'doom-linum-highlight)
 
+       ;; lsp
+       (lsp-face-highlight-textual :background dark-blue :foreground white :distant-foreground black :bold bold)
+       (lsp-face-hightlight-read   :background dark-blue :foreground white :distant-foreground black :bold bold)
+       (lsp-face-hightlight-write  :background dark-blue :foreground white :distant-foreground black :bold bold)
+
        ;; magit
        (magit-bisect-bad        :foreground red)
        (magit-bisect-good       :foreground green)
