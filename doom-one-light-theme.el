1f24d2b5e8c9a0ce7d3377785a11d2ea610797a1
Ting Zhou <zhouting@umich.edu>
2017-08-02 10:02:13 -0400

    Update light theme base colors

--- doom-one-light-theme.el
+++ doom-one-light-theme.el
@@ -30,20 +30,20 @@ determine the exact padding."
 
 ;;
 (def-doom-theme doom-one-light
-  "A dark theme inspired by Atom One Dark"
+  "A dark theme inspired by Atom One"
 
   ;; name        default   256       16
   ((bg         '("#fafafa" nil       nil            ))
    (bg-alt     '("#f0f0f0" nil       nil            ))
-   (base0      '("#1b2229" "black"   "black"        ))
-   (base1      '("#1c1f24" "#1e1e1e" "brightblack"  ))
-   (base2      '("#202328" "#2e2e2e" "brightblack"  ))
-   (base3      '("#23272e" "#262626" "brightblack"  ))
-   (base4      '("#3f444a" "#3f3f3f" "brightblack"  ))
-   (base5      '("#5b6268" "#525252" "brightblack"  ))
-   (base6      '("#73797e" "#6b6b6b" "brightblack"  ))
-   (base7      '("#9ca0a4" "#979797" "brightblack"  ))
-   (base8      '("#dfdfdf" "#dfdfdf" "white"        ))
+   (base0      '("#f0f0f0" "#f0f0f0" "white"        ))
+   (base1      '("#e7e7e7" "#e7e7e7" "brightblack"  ))
+   (base2      '("#dfdfdf" "#dfdfdf" "brightblack"  ))
+   (base3      '("#c6c7c7" "#c6c7c7" "brightblack"  ))
+   (base4      '("#9ca0a4" "#9ca0a4" "brightblack"  ))
+   (base5      '("#383a42" "#424242" "brightblack"  ))
+   (base6      '("#202328" "#2e2e2e" "brightblack"  ))
+   (base7      '("#1c1f24" "#1e1e1e" "brightblack"  ))
+   (base8      '("#1b2229" "black"   "black"        ))
    (fg         '("#383a42" "#424242" "black"  ))
    (fg-alt     '("#c6c7c7" "#c7c7c7" "brightblack"        ))
 
@@ -61,9 +61,9 @@ determine the exact padding."
    (dark-cyan  '("#005478" "#005478" "cyan"         ))
 
    ;; face categories -- required for all themes
-   (highlight      (doom-lighten blue 0.2))
+   (highlight      blue)
    (vertical-bar   base2)
-   (selection      (doom-darken bg 0.08))
+   (selection      dark-blue)
    (builtin        magenta)
    (comments       (if doom-one-light-brighter-comments dark-cyan base5))
    (doc-comments   (doom-darken (if doom-one-light-brighter-comments dark-cyan base5) 0.25))
@@ -76,7 +76,7 @@ determine the exact padding."
    (strings        green)
    (variables      (doom-darken magenta 0.36))
    (numbers        orange)
-   (region         `(,(doom-darken (car bg-alt) 0.1) ,@(doom-darken (cdr base8) 0.3)))
+   (region         `(,(doom-darken (car bg-alt) 0.1) ,@(doom-darken (cdr base0) 0.3)))
    (error          red)
    (warning        yellow)
    (success        green)
@@ -96,66 +96,25 @@ determine the exact padding."
    (modeline-bg
     (if -modeline-bright
         (doom-darken blue 0.475)
-      `(,(car bg-alt) ,@(cdr base8))))
+      `(,(car bg-alt) ,@(cdr base0))))
    (modeline-bg-l
     (if -modeline-bright
         (doom-darken blue 0.45)
-      `(,(doom-darken (car bg) 0.125) ,@(cdr base8))))
+      `(,(doom-darken (car bg) 0.125) ,@(cdr base0))))
    (modeline-bg-inactive   (doom-darken bg 0.1))
-   (modeline-bg-inactive-l `(,(car bg) ,@(cdr base7))))
+   (modeline-bg-inactive-l `(,(car bg) ,@(cdr base1))))
 
   ;; --- extra faces ------------------------
-  ((lazy-highlight :background violet :foreground bg :distant-foreground base0 :bold bold)
-
-   ;; evil-mode
-   (evil-search-highlight-persist-highlight-face :background violet)
-
-   ;; evil-snipe
-   (evil-snipe-first-match-face :foreground base0 :background magenta)
-
-   (cursor :background magenta)
-
-   (show-paren-match :background red :foreground bg :weight 'bold)
-
-   ;; helm
-   (helm-selection :foreground base0 :background bg-alt)
-   (helm-match     :foreground magenta :bold t :underline t)
-   (helm-swoop-target-line-face :foreground magenta :inverse-video t)
-
-   ;; ivy
-   (ivy-current-match :background bg-alt)
-   (ivy-minibuffer-match-face-1 :background bg :weight 'bold :underline t)
-   (swiper-line-face :background bg-alt)
-   (swiper-match-face-2 :background bg :weight 'bold :foreground magenta :underline t)
-
-   ;; company
-   (company-preview-common :background bg-alt :foreground magenta)
-   (company-tooltip-common :inherit 'company-tooltip :weight 'bold :underline nil)
-   (company-tooltip-common-selection :inherit 'company-tooltip-selection :weight 'bold :underline nil)
-
-   ;; neotree
-   (neo-dir-link-face   :foreground cyan)
-   (neo-expand-btn-face :foreground magenta)
-
-   ;; yasnippet
-   (yas-field-highlight-face :background bg-alt)
-
-   ;; magit
-   (magit-blame-heading     :foreground orange :background bg-alt)
-   (magit-diff-removed :foreground (doom-darken red 0.2) :background (doom-blend red bg 0.1))
-   (magit-diff-removed-highlight :foreground red :background (doom-blend red bg 0.2) :bold bold)
-
-   (font-lock-comment-face
+  ((font-lock-comment-face
     :foreground comments
     :background (if doom-one-light-comment-bg (doom-darken bg-alt 0.095)))
    (font-lock-doc-face
     :inherit 'font-lock-comment-face
     :foreground doc-comments)
 
-   (linum :inherit 'default :foreground base8 :distant-foreground nil :bold nil)
-   (nlinum-hl-face :foreground base5)
+   (linum :inherit 'default :foreground base4 :distant-foreground nil :bold nil)
+   (line-number-current-line :inherit 'hl-line :foreground base7 :distant-foreground nil :bold nil)
 
-   (isearch :foreground bg :background magenta)
 
    (doom-modeline-bar :background (if -modeline-bright modeline-bg highlight))
 
@@ -168,7 +127,6 @@ determine the exact padding."
    (mode-line-emphasis
     :foreground (if -modeline-bright base8 highlight))
 
-   ;; solaire-mode
    (solaire-mode-line-face
     :inherit 'mode-line
     :background modeline-bg-l
@@ -177,9 +135,11 @@ determine the exact padding."
     :inherit 'mode-line-inactive
     :background modeline-bg-inactive-l
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive-l)))
-   (solaire-hl-line-face :background bg-alt)
-   (solaire-line-number-face :background bg)
-   (solaire-default-face :background bg)
+
+   ;; magit
+   (magit-blame-heading     :foreground orange :background bg-alt)
+   (magit-diff-removed :foreground (doom-darken red 0.2) :background (doom-blend red bg 0.1))
+   (magit-diff-removed-highlight :foreground red :background (doom-blend red bg 0.2) :bold bold)
 
    ;; --- major-mode faces -------------------
    ;; css-mode / scss-mode
