683e9df094d7d7d38e91ac319e09ed5b24a05fba
Henrik Lissner <henrik@lissner.net>
2017-08-09 17:02:59 +0200

    doom-one-light: slightly lighter vc-modified

--- themes/doom-one-light-theme.el
+++ themes/doom-one-light-theme.el
@@ -80,7 +80,7 @@ determine the exact padding."
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    base5)
+   (vc-modified    base4)
    (vc-added       green)
    (vc-deleted     red)
 
