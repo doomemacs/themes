c4babe81c5466fbea5a9e4c35673d0d27d7d5b28
Henrik Lissner <henrik@lissner.net>
2017-08-09 17:03:15 +0200

    doom-one-light: tweak line-number and hl-line faces

--- themes/doom-one-light-theme.el
+++ themes/doom-one-light-theme.el
@@ -112,8 +112,10 @@ determine the exact padding."
     :inherit 'font-lock-comment-face
     :foreground doc-comments)
 
-   (line-number :inherit 'default :foreground base4 :distant-foreground nil :bold nil)
-   (line-number-current-line :inherit 'hl-line :foreground base7 :distant-foreground nil :bold nil)
+   (line-number :inherit 'default :foreground (doom-lighten base4 0.15) :distant-foreground nil :bold nil)
+   (line-number-current-line :inherit 'hl-line :foreground base8 :distant-foreground nil :bold nil)
+   (hl-line :background base2)
+   (solaire-hl-line-face :inherit 'hl-line :background base2)
 
    (doom-modeline-bar :background (if -modeline-bright modeline-bg highlight))
 
