3eb24cd53318ed04335ce4c9e90b5d5ea03f6133
Henrik Lissner <henrik@lissner.net>
2017-09-02 19:36:11 +0200

    doom-one-light: vc-modified = orange #101

--- themes/doom-one-light-theme.el
+++ themes/doom-one-light-theme.el
@@ -80,7 +80,7 @@ determine the exact padding."
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    base4)
+   (vc-modified    orange)
    (vc-added       green)
    (vc-deleted     red)
 
