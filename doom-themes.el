8ff86e456b22ab4c28605c44e544b3ef0b4b4637
Henrik Lissner <henrik@lissner.net>
2017-12-06 13:39:45 -0500

    v2.0.9 bump

--- doom-themes.el
+++ doom-themes.el
@@ -5,8 +5,8 @@
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Created: May 22, 2016
-;; Modified: November 09, 2017
-;; Version: 2.0.8
+;; Modified: December 06, 2017
+;; Version: 2.0.9
 ;; Keywords: dark light blue atom one theme neotree icons faces nova
 ;; Homepage: https://github.com/hlissner/emacs-doom-theme
 ;; Package-Requires: ((emacs "24.4") (all-the-icons "1.0.0") (cl-lib "0.5"))
