ac226f0da9f65fda96a9a100e1de7875dd491bcb
LAI Shu-lu <chenhouwu@gmail.com>
2018-10-14 03:39:35 +0800

    fix: color for vc-modified.
    base4 is hardly to see from the background.

--- themes/doom-molokai-theme.el
+++ themes/doom-molokai-theme.el
@@ -69,7 +69,7 @@ determine the exact padding."
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    base4)
+   (vc-modified    blue)
    (vc-added       (doom-darken green 0.15))
    (vc-deleted     red)
 
