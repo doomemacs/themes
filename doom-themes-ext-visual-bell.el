76517a104e59b33a5a30a39b387266d986f84104
Thomas Fini Hansen <xen@xen.dk>
2019-09-01 22:32:35 +0200

    Use face-remap in visual bell

--- doom-themes-ext-visual-bell.el
+++ doom-themes-ext-visual-bell.el
@@ -1,30 +1,22 @@
 ;;; doom-themes-ext-visual-bell.el --- flash mode-line on error -*- lexical-binding: t; -*-
 
+(require 'face-remap)
+
 (defface doom-visual-bell '((t (:inherit error :inverse-video t)))
   "Face to use for the mode-line when `doom-themes-visual-bell-config' is used."
   :group 'doom-themes)
 
-(defvar doom-themes--bell-p nil)
 ;;;###autoload
 (defun doom-themes-visual-bell-fn ()
   "Blink the mode-line red briefly. Set `ring-bell-function' to this to use it."
-  (unless doom-themes--bell-p
-    (let ((old-remap (copy-alist face-remapping-alist)))
-      (setq doom-themes--bell-p t)
-      (setq face-remapping-alist
-            (append (delete (assq 'mode-line face-remapping-alist)
-                            face-remapping-alist)
-                    '((mode-line doom-visual-bell))))
-      (force-mode-line-update)
-      (run-with-timer 0.15 nil
-                      (lambda (remap buf)
-                        (with-current-buffer buf
-                          (when (assq 'mode-line face-remapping-alist)
-                            (setq face-remapping-alist remap
-                                  doom-themes--bell-p nil))
-                          (force-mode-line-update)))
-                      old-remap
-                      (current-buffer)))))
+  (let ((doom-themes--bell-cookie (face-remap-add-relative 'mode-line 'doom-visual-bell)))
+    (force-mode-line-update)
+    (run-with-timer 0.15 nil
+                    (lambda (cookie buf)
+                      (with-current-buffer buf
+                        (face-remap-remove-relative cookie)))
+                    doom-themes--bell-cookie
+                    (current-buffer))))
 
 ;;;###autoload
 (defun doom-themes-visual-bell-config ()
