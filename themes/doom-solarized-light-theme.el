0a2261619db50b85f4160687670c8528429c3c1d
Roey Darwish Dror <roey.ghost@gmail.com>
2019-10-05 22:09:37 +0300

    Remove unnecessary use of doom-color

--- themes/doom-solarized-light-theme.el
+++ themes/doom-solarized-light-theme.el
@@ -120,7 +120,7 @@ determine the exact padding."
    (org-block-background :background (doom-blend yellow bg 0.04))
    (org-block-begin-line :background (doom-blend yellow bg 0.08))
    (org-block-end-line :background (doom-blend yellow bg 0.08))
-   (lsp-ui-sideline-code-action :foreground (doom-color 'blue))
+   (lsp-ui-sideline-code-action :foreground blue)
 
    (font-lock-comment-face
     :slant 'italic
