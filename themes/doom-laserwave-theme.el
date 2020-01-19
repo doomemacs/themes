d97de412caec13c0a288b8affb4bbe13627d5436
ema2159 <ema2159@gmail.com>
2020-01-19 13:54:13 -0600

    Fix: Ivy current match in laserwave theme

--- themes/doom-laserwave-theme.el
+++ themes/doom-laserwave-theme.el
@@ -164,7 +164,7 @@ determine the exact padding."
 
    ;; --- extra variables ---------------------
    (paren-face-match  :foreground yellow   :background (doom-darken bg 0.2) :weight 'ultra-bold)
-   (ivy-current-match :background base7 :distant-foreground nil)
+   (ivy-current-match :background base2 :distant-foreground nil)
    (tooltip           :background bg-alt :foreground fg)
    (company-box-background :foreground fg :background bg-alt))
 
