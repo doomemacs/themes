bfb26dc0bde545ede2965bcc575eb7c74259ebdc
Henrik Lissner <henrik@lissner.net>
2020-07-24 19:46:29 -0400

    doom-dracula: darker markdown-code-face background
    The light background too much contrast and didn't mesh well with
    hl-line's background.

--- themes/doom-dracula-theme.el
+++ themes/doom-dracula-theme.el
@@ -164,7 +164,7 @@ determine the exact padding."
    ;; markdown-mode
    (markdown-markup-face :foreground base5)
    (markdown-header-face :inherit 'bold :foreground red)
-   ((markdown-code-face &override) :background (doom-lighten base3 0.05))
+   ((markdown-code-face &override) :background (doom-darken 'bg 0.075))
 
    ;; org-mode
    ((outline-1 &override) :foreground level1)
