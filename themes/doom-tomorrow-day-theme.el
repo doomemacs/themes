7eab75bf10c86db858c2e20cf57b40bd5653d5cc
Henrik Lissner <henrik@lissner.net>
2021-05-26 15:38:27 -0400

    doom-tomorrow-day: fix contrast issues
    + Corrects the grey, blue, teal, and magenta colors, to match the
      original tomorrow-day palette.
    + More contrast for foreground colors in general, in particular yellows,
      blues, comments (doc-comments too), and search highlights (used to be
      black on dark blue).
    + Less contrast for background colors in general, in particular selected
      regions and the mode-line.
    + Fix outline headings: some of them were unreadable prior to this.
    + Reorganizes the doom-tomorrow-day theme for easy imenu navigation.

--- themes/doom-tomorrow-day-theme.el
+++ themes/doom-tomorrow-day-theme.el
@@ -21,7 +21,7 @@ Can be an integer to determine the exact padding."
 
   ;; name        gui       256       16
   ((bg         '("#ffffff" "white"   "white" ))
-   (bg-alt     '("#eaeaea" nil       nil     ))
+   (bg-alt     '("#f2f2f2" nil       nil     ))
    (base0      '("#f2f2f2" "white"   "white" ))
    (base1      '("#e4e4e4" "#e4e4e4"         ))
    (base2      '("#dedede" "#cccccc"         ))
@@ -34,32 +34,32 @@ Can be an integer to determine the exact padding."
    (fg         '("#4d4d4c" "#3a3a3a" "black"))
    (fg-alt     (doom-darken fg 0.6))
 
-   (grey       '("#a5a4a5" "#999999" "silver"))
+   (grey       '("#8e908c" "#999999" "silver"))
    (red        '("#c82829" "#cc3333" "red"))
    (orange     '("#f5871f" "#ff9933" "brightred"))
    (yellow     '("#eab700" "#ffcc00" "yellow"))
    (green      '("#718c00" "#669900" "green"))
-   (blue       '("#3e999f" "#339999" "brightblue"))
-   (dark-blue  '("#4271ae" "#336699" "blue"))
-   (teal       blue) ; FIXME replace with real teal
-   (magenta    '("#c9b4cf" "#c9b4cf" "magenta"))
+   (blue       '("#4271ae" "#339999" "brightblue"))
+   (dark-blue  (doom-darken blue 0.25))
+   (teal       '("#3e999f" "#339999" "brightblue"))
+   (magenta    '("#c678dd" "#c9b4cf" "magenta"))
    (violet     '("#8959a8" "#996699" "brightmagenta"))
    (cyan       '("#8abeb7" "#8abeb7" "cyan"))
    (dark-cyan  (doom-lighten cyan 0.4))
 
    ;; face categories
-   (highlight      dark-blue)
-   (vertical-bar   base0)
-   (selection      base3)
+   (highlight      blue)
+   (vertical-bar   base3)
+   (selection      base1)
    (builtin        blue)
    (comments       grey)
-   (doc-comments   (doom-darken grey 0.1))
+   (doc-comments   (doom-darken grey 0.3))
    (constants      orange)
    (functions      blue)
    (keywords       violet)
    (methods        blue)
    (operators      fg)
-   (type           yellow)
+   (type           (doom-darken yellow 0.1))
    (strings        green)
    (variables      red)
    (numbers        orange)
@@ -67,15 +67,19 @@ Can be an integer to determine the exact padding."
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    fg-alt)
-   (vc-added       green)
+   (vc-modified    (doom-lighten yellow 0.4))
+   (vc-added       (doom-lighten green 0.4))
    (vc-deleted     red)
 
    ;; custom categories
-   (modeline-bg     `(,(doom-darken (car bg) 0.1) ,@(cdr base3)))
-   (modeline-bg-alt `(,(doom-darken (car bg) 0.14) ,@(cdr base1)))
-   (modeline-fg     base8)
-   (modeline-fg-alt comments)
+   (modeline-bg              `(,(doom-lighten (car bg-alt) 0.4) ,@(cdr base3)))
+   (modeline-bg-alt          bg)
+   (modeline-bg-inactive     `(,(doom-darken (car bg) 0.04) ,@(cdr base1)))
+   (modeline-bg-alt-inactive bg)
+   (modeline-fg              fg)
+   (modeline-fg-inactive     comments)
+   (modeline-fg-alt-inactive comments)
+
    (-modeline-pad
     (when doom-tomorrow-day-padded-modeline
       (if (integerp doom-tomorrow-day-padded-modeline)
@@ -83,41 +87,56 @@ Can be an integer to determine the exact padding."
         4))))
 
   ;; --- faces ------------------------------
-  ((doom-modeline-buffer-path       :foreground violet :bold bold)
-   (doom-modeline-buffer-major-mode :inherit 'doom-modeline-buffer-path)
-
+  (;;;; emacs
    ((line-number &override) :foreground base4)
-   ((line-number-current-line &override) :foreground blue :bold bold)
-
-   (ivy-current-match :background region :distant-foreground grey :weight 'ultra-bold)
-   (ivy-minibuffer-match-face-1
-    :foreground base5
-    :weight 'light)
-   (ivy-minibuffer-match-face-2 :inherit 'ivy-minibuffer-match-face-1 :foreground violet :weight 'ultra-bold)
-   (ivy-minibuffer-match-face-3 :inherit 'ivy-minibuffer-match-face-2 :foreground blue)
-   (ivy-minibuffer-match-face-4 :inherit 'ivy-minibuffer-match-face-2 :foreground red)
-
-   ;; rainbow-delimiters
-   (rainbow-delimiters-depth-1-face :foreground violet)
-   (rainbow-delimiters-depth-2-face :foreground blue)
-   (rainbow-delimiters-depth-3-face :foreground orange)
-   (rainbow-delimiters-depth-4-face :foreground green)
-   (rainbow-delimiters-depth-5-face :foreground magenta)
-   (rainbow-delimiters-depth-6-face :foreground yellow)
-   (rainbow-delimiters-depth-7-face :foreground teal)
-
+   ((line-number-current-line &override) :foreground base8)
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
    (mode-line-inactive
-    :background modeline-bg-alt :foreground modeline-fg-alt
-    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-alt)))
-
-   ;; org
+    :background modeline-bg-inactive :foreground modeline-fg-inactive
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
+   (mode-line-highlight :inherit 'bold :background highlight :foreground base0)
+   ;;;; doom-modeline
+   (doom-modeline-bar :background highlight)
+   (doom-modeline-buffer-path       :foreground violet :weight 'bold)
+   (doom-modeline-buffer-major-mode :inherit 'doom-modeline-buffer-path)
+   ;;;; ivy
+   (ivy-current-match :background region :distant-foreground grey :weight 'ultra-bold)
+   (ivy-minibuffer-match-face-1 :foreground base5 :weight 'light)
+   (ivy-minibuffer-match-face-2 :inherit 'ivy-minibuffer-match-face-1 :foreground violet :weight 'ultra-bold)
+   (ivy-minibuffer-match-face-3 :inherit 'ivy-minibuffer-match-face-2 :foreground blue)
+   (ivy-minibuffer-match-face-4 :inherit 'ivy-minibuffer-match-face-2 :foreground red)
+   ;;;; org <built-in>
    (org-block-begin-line         :foreground base7 :background base3 :extend t)
-
-   ;; treemacs
+   ;;;; outline <built-in>
+   ((outline-1 &override) :foreground teal)
+   ((outline-2 &override) :foreground blue)
+   ((outline-3 &override) :foreground violet)
+   ((outline-4 &override) :foreground blue)
+   ((outline-5 &override) :foreground violet)
+   ((outline-6 &override) :foreground blue)
+   ((outline-7 &override) :foreground violet)
+   ((outline-8 &override) :foreground blue)
+   ;;;; rainbow-delimiters
+   (rainbow-delimiters-depth-1-face :foreground violet)
+   (rainbow-delimiters-depth-2-face :foreground blue)
+   (rainbow-delimiters-depth-3-face :foreground green)
+   (rainbow-delimiters-depth-4-face :foreground magenta)
+   (rainbow-delimiters-depth-5-face :foreground orange)
+   (rainbow-delimiters-depth-6-face :foreground yellow)
+   (rainbow-delimiters-depth-7-face :foreground teal)
+   ;;;; solaire-mode
+   (solaire-mode-line-face :inherit 'mode-line :background modeline-bg-alt)
+   (solaire-mode-line-inactive-face
+    :inherit 'mode-line-inactive
+    :background modeline-bg-alt-inactive
+    :foreground modeline-fg-alt-inactive)
+   ;;;; treemacs
    (treemacs-git-untracked-face :foreground yellow)
+   ;;;; whitespace <built-in>
+   (whitespace-tab :background (doom-lighten base0 0.6)
+                   :foreground comments)
    )
 
   ;; --- variables --------------------------
