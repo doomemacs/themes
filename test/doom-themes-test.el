15a0198bad23daaa3749eec29793e6a4cfe2f686
Henrik Lissner <henrik@lissner.net>
2021-05-31 03:44:35 -0400

    Generate load-theme tests for each theme
    This way we expose failures in multiple themes, rather than stop at the
    first failure.

--- test/doom-themes-test.el
+++ test/doom-themes-test.el
@@ -3,15 +3,16 @@
 (defvar doom-themes-dir
   (expand-file-name "../" (file-name-directory load-file-name)))
 
-(ert-deftest doom-themes-load-themes ()
-  "TODO"
-  (mapc (lambda (file)
-          (let ((theme (intern (string-remove-suffix "-theme" (file-name-base file)))))
-            (load-theme theme t)
-            (should (custom-theme-enabled-p theme))
-            (disable-theme theme)))
-        (file-expand-wildcards
-         (expand-file-name "themes/doom-*-theme.el" doom-themes-dir))))
+(mapc
+ (lambda (file)
+   (let ((theme (intern (string-remove-suffix "-theme" (file-name-base file)))))
+     (eval `(ert-deftest ,(intern (format "doom-themes-load-%s" theme)) ()
+              ,(format "Make sure `%s-theme' loads without issue" theme)
+              (load-theme ',theme t)
+              (should (custom-theme-enabled-p ',theme)))
+           t)))
+ (file-expand-wildcards
+  (expand-file-name "themes/doom-*-theme.el" doom-themes-dir)))
 
 (provide 'doom-themes-test)
 ;;; doom-themes-test.el ends here
