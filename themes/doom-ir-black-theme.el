0e3a30cb6c6bb36d95e8e355bffcfa12e3bc56de
Henrik Lissner <henrik@lissner.net>
2021-06-04 12:50:22 -0400

    Clean up doom-ir-black
    Remove redundancies, add comment header, and organize by plugin/mode
    with outline headings.

--- themes/doom-ir-black-theme.el
+++ themes/doom-ir-black-theme.el
@@ -1,9 +1,27 @@
-;;; doom-ir-black-theme.el --- port of the original ir_black theme -*- lexical-bindings: t; no-byte-compile: t; -*-
+;;; doom-ir-black-theme.el --- port of ir_black -*- lexical-bindings: t; no-byte-compile: t; -*-
+;;
+;; Author: Todd Werth <todd@infinite.red>
+;; Ported by: Kevin Kainan Li <legendre6891@users.noreply.github.com>
+;; Created: January 10, 2019
+;; Version: 2.0.0
+;; Keywords: custom themes, faces
+;; Homepage: https://github.com/hlissner/emacs-doom-themes
+;; Package-Requires: ((emacs "25.1") (cl-lib "0.5") (doom-themes "2.2.1"))
+;;
+;;; Commentary:
+;;
+;; Ported from the original ir_black theme in hlissner/emacs-doom-themes#255.
+;;
+;;; Code:
+
 (require 'doom-themes)
 
+
 ;;
+;;; Variables
+
 (defgroup doom-ir-black-theme nil
-  "Options for doom-themes"
+  "Options for the `doom-ir-black' theme."
   :group 'doom-themes)
 
 (defcustom doom-ir-black-brighter-comments nil
@@ -11,25 +29,33 @@
   :group 'doom-ir-black-theme
   :type 'boolean)
 
-(defcustom doom-ir-black-comment-bg doom-ir-black-brighter-comments
-  "If non-nil, comments will have a subtle, darker background. Enhancing their
-legibility."
-  :group 'doom-ir-black-theme
-  :type 'boolean)
-
 (defcustom doom-ir-black-padded-modeline doom-themes-padded-modeline
-  "If non-nil, adds a 4px padding to the mode-line. Can be an integer to
-determine the exact padding."
+  "If non-nil, adds a 4px padding to the mode-line.
+Can be an integer to determine the exact padding."
   :group 'doom-ir-black-theme
   :type '(or integer boolean))
 
+
 ;;
+;;; Theme definition
+
 (def-doom-theme doom-ir-black
-  "A dark theme inspired by Atom One Dark"
+  "A port of the original IR Black colorscheme."
 
   ;; name        default   256       16
   ((bg         '("#000000" "black"   "black"        ))
+   (fg         '("#f6f3e8" "#f6f3e8" "brightwhite"  ))
+
+   ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
+   ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
+   ;; or region), especially when paired with the `doom-darken', `doom-lighten',
+   ;; and `doom-blend' helper functions.
    (bg-alt     '("#121212" "black"   "black"        ))
+   (fg-alt     '("#5B6268" "#2d2d2d" "white"        ))
+
+   ;; These should represent a spectrum from bg to fg, where base0 is a starker
+   ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
+   ;; dark grey, base0 should be white and base8 should be black.
    (base0      '("#1B2229" "black"   "black"        ))
    (base1      '("#1c1f24" "#1e1e1e" "brightblack"  ))
    (base2      '("#202328" "#2e2e2e" "brightblack"  ))
@@ -39,13 +65,8 @@ determine the exact padding."
    (base6      '("#73797e" "#6b6b6b" "brightblack"  ))
    (base7      '("#9ca0a4" "#979797" "brightblack"  ))
    (base8      '("#DFDFDF" "#dfdfdf" "white"        ))
-   (extra-1    '("#3D3D3D" "#3D3D3D" "brightblack"  ))
-   (extra-2    '("#3D3D3D" "#3D3D3D" "brightblack"  ))
 
-   (fg         '("#f6f3e8" "#f6f3e8" "brightwhite"  ))
-   (fg-alt     '("#5B6268" "#2d2d2d" "white"        ))
    (white      '("#ffffff" "#ffffff" "white"))
-
    (grey       base4)
    (red        '("#ff6c60" "#ff6c60" "red"          ))
    (orange     '("#E9C062" "#E9C062" "brightred"    ))
@@ -86,35 +107,21 @@ determine the exact padding."
    (vc-deleted     red)
 
    ;; custom categories
-   (hidden     `(,(car bg) "black" "black"))
    (-modeline-pad
     (when doom-ir-black-padded-modeline
       (if (integerp doom-ir-black-padded-modeline) doom-ir-black-padded-modeline 4)))
 
-   (modeline-fg     white)
-   (modeline-fg-alt base5)
-
-   (modeline-bg base4)
-   (modeline-bg-l base4)
-   (modeline-bg-inactive base3)
-   (modeline-bg-inactive-l base3))
+   (modeline-fg          white)
+   (modeline-fg-alt      base5)
+   (modeline-bg          base4)
+   (modeline-bg-inactive base3))
 
 
-  ;; --- extra faces ------------------------
-  ((elscreen-tab-other-screen-face :background "#353a42" :foreground "#1e2022")
-
-   (evil-goggles-default-face :inherit 'region :background (doom-blend region bg 0.5))
-
+  ;;;; Base theme face overrides
+  (((font-lock-comment-face &override)
+    :background (if doom-ir-black-brighter-comments (doom-lighten bg 0.05)))
    ((line-number &override) :foreground base5)
    ((line-number-current-line &override) :foreground "#FFFF00" :weight 'bold)
-
-   (font-lock-comment-face
-    :foreground comments
-    :background (if doom-ir-black-comment-bg (doom-lighten bg 0.05)))
-   (font-lock-doc-face
-    :inherit 'font-lock-comment-face
-    :foreground doc-comments)
-
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
@@ -122,35 +129,22 @@ determine the exact padding."
     :background modeline-bg-inactive :foreground modeline-fg-alt
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
 
-   (solaire-mode-line-face
-    :inherit 'mode-line
-    :background modeline-bg-l
-    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-l)))
-   (solaire-mode-line-inactive-face
-    :inherit 'mode-line-inactive
-    :background modeline-bg-inactive-l
-    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive-l)))
-
-   ;; ivy-mode
-   (ivy-current-match :background dark-blue :distant-foreground base0 :weight 'normal)
-
-   ;; --- major-mode faces -------------------
-   ;; css-mode / scss-mode
+   ;;;; css-mode <built-in> / scss-mode
    (css-proprietary-property :foreground orange)
    (css-property             :foreground green)
    (css-selector             :foreground blue)
-
-   ;; markdown-mode
+   ;;;; elscreen
+   (elscreen-tab-other-screen-face :background "#353a42" :foreground "#1e2022")
+   ;;;; evil
+   (evil-goggles-default-face :inherit 'region :background (doom-blend region bg 0.5))
+   ;;;; ivy
+   (ivy-current-match :background dark-blue :distant-foreground base0 :weight 'normal)
+   ;;;; markdown-mode
    (markdown-markup-face :foreground base5)
    (markdown-header-face :inherit 'bold :foreground red)
-   (markdown-code-face :background (doom-lighten base3 0.05))
+   (markdown-code-face :background (doom-lighten base3 0.05)))
 
-   ;; org-mode
-   (org-hide :foreground hidden)
-   (solaire-org-hide-face :foreground hidden))
-
-
-  ;; --- extra variables ---------------------
+  ;;;; Base theme variable overrides-
   ;; ()
   )
 
