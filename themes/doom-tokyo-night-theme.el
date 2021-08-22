cdc1069c3cd417f04bbefb46d7835708766d31ac
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-08-21 23:38:45 -0400

    Cleaned up some comments.

--- themes/doom-tokyo-night-theme.el
+++ themes/doom-tokyo-night-theme.el
@@ -34,21 +34,21 @@
   ; "#f7768e" "#f7768e" => This keyword, HTML elements, Regex group symbol, CSS units, Terminal Red
   ; "#ff9e64" "#ff9e64" => Number and Boolean constants, Language support constants
   ; "#e0af68" "#e0af68" => Function parameters, Regex character sets, Terminal Yellow
-  ; "#9ece6a"           => Strings, CSS class names
-  ; "#73daca"           => Object literal keys, Markdown links, Terminal Green
-  ; "#b4f9f8"           => Regex literal strings
-  ; "#2ac3de"           => Language support functions, CSS HTML elements
-  ; "#7dcfff"           => Object properties, Regex quantifiers and flags, Markdown headings, Terminal Cyan, Markdown code, Import/export keywords
-  ; "#7aa2f7"           => Function names, CSS property names, Terminal Blue
-  ; "#bb9af7"           => Control Keywords, Storage Types, Regex symbols and operators, HTML Attributes, Terminal Magenta
-  ; "#c0caf5"           => Variables, Class names, Terminal White
-  ; "#a9b1d6"           => Editor foreground
-  ; "#9aa5ce"           => Markdown Text, HTML Text
-  ; "#cfc9c2"           => Parameters inside functions (semantic highlighting only)
-  ; "#565f89"           => Comments
-  ; "#414868"           => Terminal black
-  ; "#24283b"           => Editor background (Storm)
-  ; "#1a1b26"           => Editor background (Night)
+  ; "#9ece6a" "#9ece6a" => Strings, CSS class names
+  ; "#73daca" "#73daca" => Object literal keys, Markdown links, Terminal Green
+  ; "#b4f9f8" "#b4f9f8" => Regex literal strings
+  ; "#2ac3de" "#2ac3de" => Language support functions, CSS HTML elements
+  ; "#7dcfff" "#7dcfff" => Object properties, Regex quantifiers and flags, Markdown headings, Terminal Cyan, Markdown code, Import/export keywords
+  ; "#7aa2f7" "#7aa2f7" => Function names, CSS property names, Terminal Blue
+  ; "#bb9af7" "#bb9af7" => Control Keywords, Storage Types, Regex symbols and operators, HTML Attributes, Terminal Magenta
+  ; "#c0caf5" "#c0caf5" => Variables, Class names, Terminal White
+  ; "#a9b1d6" "#a9b1d6" => Editor foreground
+  ; "#9aa5ce" "#9aa5ce" => Markdown Text, HTML Text
+  ; "#cfc9c2" "#cfc9c2" => Parameters inside functions (semantic highlighting only)
+  ; "#565f89" "#565f89" => Comments
+  ; "#414868" "#414868" => Terminal black
+  ; "#24283b" "#24283b" => Editor background (Storm)
+  ; "#1a1b26" "#1a1b26" => Editor background (Night)
 
   ;; name        default   256       16
   ((bg         '("#1a1b26" nil       nil            ))
@@ -216,12 +216,6 @@
    ;; --- Major-Mode Faces -------------------
    ;;; js2-mode
    (js2-function-param :foreground yellow)
-   ;(js2-jsdoc-html-tag-delimiter :foreground yellow)
-   ;(js2-jsdoc-html-tag-name :foreground dark-blue)
-   ;(js2-jsdoc-value :foreground yellow)
-   ;(js2-private-function-call :foreground cyan)
-   ;(js2-private-member :foreground orange)
-   ;(js2-object-property-access :foreground dark-cyan)
    (js2-object-property :foreground dark-cyan)
 
    ;;; typescript-mode
