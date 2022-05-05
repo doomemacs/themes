e9bdd137116fa2037ed60037b8421cf68c64888d
Henrik Lissner <git@henrik.io>
2022-05-05 15:27:23 +0200

    dev(ci): update commit linter config
    To adapt to efforts made upstream to generalize Doom's CI API.
    
    Also fixes a 'Keyword argument :X not one of (:type)' error.
    
    Ref: doomemacs/doomemacs@4bae9ffa4769

--- .github/ci.el
+++ .github/ci.el
@@ -9,30 +9,16 @@
 
 ;;; Scopes
 (add-to-list 'doom-cli-commit-scopes "base")
-(add-to-list 'doom-cli-commit-scopes #'ci-theme-scope)
-(add-to-list 'doom-cli-commit-scopes #'ci-extension-scope)
-
-
-;;; Helpers
-(cl-defun ci-theme-scope (scope (&key type))
-  "Only allow scopes for themes that actually exist."
-  (when scope
-    (cl-loop with default-directory = (dir!)
-             for file in (doom-files-in "../themes/" :match "-theme\\.el$")
-             if (file-name-base file)
-             if (string-remove-prefix "doom-" it)
-             if (string-remove-suffix "-theme" it)
-             if (equal it scope)
-             return t)))
-
-(cl-defun ci-extension-scope (scope (&key type))
-  "Only allow scopes for extensions that actually exist."
-  (when scope
-    (cl-loop with default-directory = (dir!)
-             for file in (doom-files-in "../extensions/" :match "\\.el$")
-             if (file-name-base file)
-             if (string-remove-prefix "doom-themes-" it)
-             if (equal it scope)
-             return t)))
+(cl-loop with default-directory = (dir!)
+         for file in (doom-files-in "../themes/" :match "-theme\\.el$")
+         if (file-name-base file)
+         if (string-remove-prefix "doom-" it)
+         if (string-remove-suffix "-theme" it)
+         do (add-to-list 'doom-cli-commit-scopes it))
+(cl-loop with default-directory = (dir!)
+         for file in (doom-files-in "../extensions/" :match "\\.el$")
+         if (file-name-base file)
+         if (string-remove-prefix "doom-themes-" it)
+         do (add-to-list 'doom-cli-commit-scopes it))
 
 ;;; ci.el ends here
