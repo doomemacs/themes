245e2e9454a37d1f06653359f57f49963f063751
Chris Elrod <elrodc@gmail.com>
2023-09-08 17:31:17 -0400

    fix(acario-dark): 'nil value is invalid' warnings
    Addresses the following warnings:
    
    ```
    Warning: setting attribute ‘:foreground’ of face ‘mode-line’: nil value is invalid, use ‘unspecified’ instead.
    Warning: setting attribute ‘:background’ of face ‘font-lock-comment-face’: nil value is invalid, use ‘unspecified’ instead.
    ```
    
    Ref: #753

--- themes/doom-acario-dark-theme.el
+++ themes/doom-acario-dark-theme.el
@@ -108,7 +108,7 @@ determine the exact padding."
     (when doom-acario-dark-padded-modeline
       (if (integerp doom-acario-dark-padded-modeline) doom-acario-dark-padded-modeline 4)))
 
-   (modeline-fg     nil)
+   (modeline-fg     'unspecified)
    (modeline-fg-alt base7)
 
    (modeline-bg
@@ -127,7 +127,9 @@ determine the exact padding."
   ((font-lock-comment-face
     :slant 'italic
     :foreground comments
-    :background (if doom-acario-dark-comment-bg (doom-lighten bg 0.05)))
+    :background (if doom-acario-dark-comment-bg
+                    (doom-lighten bg 0.05)
+                  'unspecified))
    (font-lock-doc-face
     :inherit 'font-lock-comment-face
     :foreground doc-comments)
