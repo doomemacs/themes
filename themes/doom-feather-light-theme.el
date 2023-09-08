a0508f3d07529cae9c19d47761d77edca9b758bd
Brian Leung <29217594+leungbk@users.noreply.github.com>
2023-09-08 14:28:40 -0700

    fix(feather-light,flatwhite,one-light): 'nil value is invalid' warnings

--- themes/doom-feather-light-theme.el
+++ themes/doom-feather-light-theme.el
@@ -223,8 +223,8 @@ Can be an integer to determine the exact padding."
    ;;;; wgrep <built-in>
    (wgrep-face :background base1)
    ;;;; whitespace
-   ((whitespace-tab &override)         :background (unless (default-value 'indent-tabs-mode) base0))
-   ((whitespace-indentation &override) :background (if (default-value 'indent-tabs-mode) base0)))
+   ((whitespace-tab &override)         :background (if (not (default-value 'indent-tabs-mode)) base0 'unspecified))
+   ((whitespace-indentation &override) :background (if (default-value 'indent-tabs-mode) base0 'unspecified)))
 
   ;;;; Base theme variable overrides-
   ()
