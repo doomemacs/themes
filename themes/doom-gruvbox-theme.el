2c794a09b023bda09d2f36a7be80684f4e416d88
Acid Bong <acidbong@tilde.club>
2024-07-12 20:10:38 +0300

    tweak(gruvbox): use magenta from the official palette
    Fix: #780

--- themes/doom-gruvbox-theme.el
+++ themes/doom-gruvbox-theme.el
@@ -71,7 +71,7 @@ background contrast. All other values default to \"medium\"."
    ;; Standardized official colours from gruvbox
    (grey        '("#928374" "#909090" "brightblack"))   ; gray
    (red         '("#fb4934" "#e74c3c" "red"))           ; bright-red
-   (magenta     '("#cc241d" "#cc241d" "magenta"))       ; red
+   (magenta     '("#b16286" "#b16286" "magenta"))       ; red
    (violet      '("#d3869b" "#d3869b" "brightmagenta")) ; bright-purple
    (orange      '("#fe8019" "#fd971f" "orange"))        ; bright-orange
    (yellow      '("#fabd2f" "#fabd2f" "yellow"))        ; bright-yellow
